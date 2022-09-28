5a7c7477a3cf81599eb3f7bf141229ed4a584f4b
Tianqing Gao <tianqing.gao@student.adelaide.edu.au>
2022-09-28 12:25:37 +0930

    update some components

--- src/index.js
+++ src/index.js
@@ -8,8 +8,48 @@ import ReactDOM from "react-dom";
 // classname instead of class
 // close every element
 
-const Newcomponent = () => {
-  return React.createElement("h1", {}, "hello");
+import "./index.css";
+
+//setup vars
+
+const books = [
+  {
+    id: 0,
+    img: "https://images-na.ssl-images-amazon.com/images/I/71fXg7UX5+L._AC._SR360,460.jpg",
+    author: "Author Name2",
+    title: "Book Title2",
+  },
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51eO828JvzL._AC._SR360,460.jpg",
+    author: "Author Name",
+    title: "Book Title",
+  },
+];
+
+const Booklist = () => {
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        //const { img, author, title } = book;
+        // return <Book key={book.id} book={book}></Book>;
+
+        // spread operater to pass as prop
+        return <Book key={book.id} {...book}></Book>;
+      })}
+    </section>
+  );
 };
 
-ReactDOM.render(<Newcomponent />, document.getElementById("root"));
+const Book = (props) => {
+  const { img, author, title } = props;
+  return (
+    <article className="book">
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      <img src={img} alt="" />
+    </article>
+  );
+};
+
+ReactDOM.render(<Booklist />, document.getElementById("root"));
